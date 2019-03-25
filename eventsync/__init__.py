6486b522dbdff5115c88a5217fd6206e031bf011
Stephen Onnen <stephen.onnen@gmail.com>
2019-03-25 11:46:13 -0500

    Added comments on html templates

--- eventsync/__init__.py
+++ eventsync/__init__.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route("/", methods=["GET"])
 def index():
-    return render_template("base.html")
+    return render_template("index.html")
 
 
 if __name__ == "__main__":
