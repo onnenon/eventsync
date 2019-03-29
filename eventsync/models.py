70c61d93721eecc798746e735ae2965dde1380cb
Stephen Onnen <stephen.onnen@gmail.com>
2019-03-29 10:58:20 -0500

    Added Users table to models

---
+++ eventsync/models.py
@@ -0,0 +1,14 @@
+"""
+Database Models go here
+"""
+
+from flask_sqlalchemy import SQLAlchemy
+
+
+db = SQLAlchemy()
+
+
+class User(db.Model):
+    __tablename__ = "users"
+
+    username = db.Column(db.String(30), nullable=False, primary_key=True)
