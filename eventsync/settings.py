d31d7aa9776a9e6bbf62e250a4be73aba59a81dd
Stephen Onnen <stephen.onnen@gmail.com>
2019-04-04 14:47:16 -0500

    Added vagrantfile

--- eventsync/settings.py
+++ eventsync/settings.py
@@ -8,10 +8,10 @@ SECRET_KEY = os.urandom(24)
 
 # DATABASE
 SQLALCHEMY_DATABASE_URI = "postgresql://{}:{}@{}:{}/{}".format(
-    os.getenv("DB_USERNAME"),
-    os.getenv("DB_PASSWORD"),
+    os.getenv("DB_USERNAME", "eventadmin"),
+    os.getenv("DB_PASSWORD", "password"),
     os.getenv("DB_HOST", "localhost"),
     os.getenv("DB_PORT", "5432"),
-    os.getenv("DB_NAME"),
+    os.getenv("DB_NAME", "eventSyncDB"),
 )
 SQLALCHEMY_TRACK_MODIFICATIONS = False
