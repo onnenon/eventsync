73d02fa09ae39401701a60814e8bbc741264ebc2
Stephen Onnen <stephen.onnen@gmail.com>
2019-04-04 22:27:33 -0500

    Made DB set by environment variable

--- eventsync/settings.py
+++ eventsync/settings.py
@@ -9,14 +9,15 @@ LOGGER = spylogger.get_logger()
 SECRET_KEY = os.urandom(24)
 
 # DATABASE
-# SQLALCHEMY_DATABASE_URI = "postgresql://{}:{}@{}:{}/{}".format(
-#     os.getenv("DB_USERNAME", "eventadmin"),
-#     os.getenv("DB_PASSWORD", "password"),
-#     os.getenv("DB_HOST", "localhost"),
-#     os.getenv("DB_PORT", "5432"),
-#     os.getenv("DB_NAME", "eventSyncDB"),
-# )
+postgresql = "postgresql://{}:{}@{}:{}/{}".format(
+    os.getenv("DB_USERNAME", "eventadmin"),
+    os.getenv("DB_PASSWORD", "password"),
+    os.getenv("DB_HOST", "localhost"),
+    os.getenv("DB_PORT", "5432"),
+    os.getenv("DB_NAME", "eventSyncDB"),
+)
+sqlite = f"sqlite:///app.db"
 
-SQLALCHEMY_DATABASE_URI = f"sqlite:///app.db"
+SQLALCHEMY_DATABASE_URI = postgresql if os.getenv("DB") == "postgres" else sqlite
 
 SQLALCHEMY_TRACK_MODIFICATIONS = False
