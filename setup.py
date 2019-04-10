d51596ece38a256f96343557f49abd4d66e59835
Stephen Onnen <stephen.onnen@gmail.com>
2019-04-10 11:09:09 -0500

    Added setup.py file

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="eventsync",
+    version="0.0.1",
+    author="COM S 319 Team 15",
+    description="A program for organizing events."
+    license="MIT",
+    url="https://git.linux.iastate.edu/319spring19projectrepositories/taghreed/group-15.git"
+    packages=find_packages(),
+)
