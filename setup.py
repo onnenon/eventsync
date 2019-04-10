57ad81bcebdbf8e4e5152748350d7a4cb2e58a31
srekic <srekic@iastate.edu>
2019-04-10 11:27:04 -0500

    fixed typo

--- setup.py
+++ setup.py
@@ -4,8 +4,8 @@ setup(
     name="eventsync",
     version="0.0.1",
     author="COM S 319 Team 15",
-    description="A program for organizing events."
+    description="A program for organizing events.",
     license="MIT",
-    url="https://git.linux.iastate.edu/319spring19projectrepositories/taghreed/group-15.git"
+    url="https://git.linux.iastate.edu/319spring19projectrepositories/taghreed/group-15.git",
     packages=find_packages(),
 )
